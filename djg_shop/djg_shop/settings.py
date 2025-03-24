49472f93880f68769b85496f8156e8f246b23b38
IsaacSemb <isaacsemb1996@gmail.com>
2025-03-24 20:59:04 +0000

    switched database from sqlite to mysql and run migrations
    provided credentials for dummy database
    Using phpadmin as the user interface for the database

--- djg_shop/djg_shop/settings.py
+++ djg_shop/djg_shop/settings.py
@@ -79,8 +79,13 @@ WSGI_APPLICATION = 'djg_shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djg_shop_db',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '1234',
     }
 }
 
