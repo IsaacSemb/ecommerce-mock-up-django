790628e50831c467df57ab43d450ad05851bc045
IsaacSemb <isaacsemb1996@gmail.com>
2025-03-24 00:46:35 +0000

    Brainstorming generic type to know how it works and what each column means
    the included columns include
    
    taggedItem_id unique table identifer
    tag_id - this id the foreign key to the tags
    content_type - this is the django model with djangos registry
    objectid - this is the exact  object wiith in that model

--- djg_shop/tags/models.py
+++ djg_shop/tags/models.py
@@ -3,8 +3,16 @@ from django.db import models
 # Create your models here.
 
 class Tag(models.Model):
-    label = models.CharField(verbose_name='tag label', max_length=255)
+    tag_name = models.CharField(verbose_name='name of the tag', max_length=255)
     
+
+notes_taggedItem = """
+    for tagged object we cdant just import say products 
+    cause the a dependency will be created between the two apps (shop and tags)
+    we can use a generic model called content type from the Content type class   
+    
+
+"""
     
 class TaggedItem(models.Model):
     tag = models.ForeignKey(Tag, verbose_name="which tag", on_delete=models.CASCADE)
