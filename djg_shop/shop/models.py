f2898e5f9231cb22086a320edd5257a94edfc1c7
IsaacSemb <isaacsemb1996@gmail.com>
2025-03-24 01:43:02 +0000

    Add slug field to shop.product model

--- djg_shop/shop/models.py
+++ djg_shop/shop/models.py
@@ -28,6 +28,7 @@ class Product(models.Model):
     created_at = models.DateTimeField(verbose_name="date and time created", auto_now_add=True)
     last_update = models.DateTimeField(verbose_name="date and time updated", auto_now=True)
     category = models.ForeignKey('Category', verbose_name="Product Category", on_delete=models.PROTECT)
+    slug = models.SlugField()
     
     # category
     # slug
