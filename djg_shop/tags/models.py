8bfab8e19828e20503b0546a4c5e6f255d9be79b
IsaacSemb <isaacsemb1996@gmail.com>
2025-03-24 00:01:55 +0000

    created models for the tag app
    this app is intended to be dissociated from everything other app
    hence we have to make it generic
    we have to give it generic relationship to avoid importing stuff

--- djg_shop/tags/models.py
+++ djg_shop/tags/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Tag(models.Model):
+    label = models.CharField(verbose_name='tag label', max_length=255)
+    
+    
+class TaggedItem(models.Model):
+    tag = models.ForeignKey(Tag, verbose_name="which tag", on_delete=models.CASCADE)
+        
