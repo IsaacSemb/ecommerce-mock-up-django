474445bf8cdc204a8f29e57c8e4853097d104413
IsaacSemb <isaacsemb1996@gmail.com>
2025-03-25 20:06:15 +0000

    Playing around with the ORM queries for temporary learning

--- djg_shop/shop/views.py
+++ djg_shop/shop/views.py
@@ -2,15 +2,13 @@ from django.shortcuts import render
 
 from .models import Product
 
-# Create your views here.
 def playground(request):
-    
-    
+        
     context = {
         
         'all_products':Product.objects.all()[:5]
         
     }
     
-    
+
     return render(request, 'playground.html', context)
