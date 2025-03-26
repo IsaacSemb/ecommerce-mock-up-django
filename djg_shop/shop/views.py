f3e14c00d51a9d98b8afd67736bb89f15a4ad8bf
IsaacSemb <isaacsemb1996@gmail.com>
2025-03-26 19:28:38 +0000

    create browsable API from django rest frame work

--- djg_shop/shop/views.py
+++ djg_shop/shop/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, HttpResponse
 from django.db.models.aggregates import Count, Avg, Max, Min, Sum
 from django.db.models import Value, F, Func
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from .models import Order, OrderItem, Product, Customer
 
+@api_view()
 def product_list(request):
-    return HttpResponse('ok')
+    return Response('ok')
