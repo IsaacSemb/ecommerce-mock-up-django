0b89aea80278af09e645c291e63f2146325ed7ad
IsaacSemb <isaacsemb1996@gmail.com>
2025-03-26 19:38:49 +0000

    change product id api to receive only integers

--- djg_shop/shop/urls.py
+++ djg_shop/shop/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('products/', views.all_products),
-    path('products/<id>', views.product_detail)
+    path('products/<int:id>', views.product_detail)
 ]
