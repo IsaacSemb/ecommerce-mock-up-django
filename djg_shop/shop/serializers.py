75a5e7ecf1292772915ef1a54176c00bd9f5406f
IsaacSemb <isaacsemb1996@gmail.com>
2025-03-26 20:05:06 +0000

    creating cutom serializers
    changing serializer field names with the source argument
    computed fields using the method field function

--- djg_shop/shop/serializers.py
+++ djg_shop/shop/serializers.py
@@ -1,16 +1,27 @@
+from decimal import Decimal
 from rest_framework import serializers
+
+from .models import Product
 
 
 notes = """
 
 serializers have field names just like the models fields
 
+you fields that arent in your application
+describe a funtion to get the result and use method field and pass the function
+
+renaming fields, you can rename fields and state the source name from the model
 
 """
 
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     product_name = serializers.CharField(max_length=255)
-    unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
+    price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
+    price_with_tax = serializers.SerializerMethodField('calculate_tax')
+    
+    def calculate_tax(self, product:Product):
+        return product.unit_price * Decimal(1.1)
 
 
--- djg_shop/shop/views.py
+++ djg_shop/shop/views.py
@@ -1,16 +1,43 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import get_object_or_404
 from django.db.models.aggregates import Count, Avg, Max, Min, Sum
 from django.db.models import Value, F, Func
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from .models import Order, OrderItem, Product, Customer
+from .models import Product
+from .serializers import ProductSerializer
 
 @api_view()
 def all_products(request):
-    return Response('ok')
+    
+    # get data
+    query_set = Product.objects.all()
+    
+    # create serializer for the data
+    serializer = ProductSerializer(query_set, many=True) # pass many=True so the serializer knows to iterate
+    
+    data = serializer.data
+    
+    return Response(data)
 
+
+notes = """
+
+retrieve the object and pass it to the serializer
+
+
+"""
 
 @api_view()
 def product_detail(request,id):
-    return Response(id)
+    
+    # get the product you want to get 
+    product = get_object_or_404(Product, pk=id)
+    
+    # srialisze it using the responsible serializer
+    serializer = ProductSerializer(product)
+
+    # get the data from it and pass it and pass it as a response
+    data = serializer.data
+    
+    return Response(data)
