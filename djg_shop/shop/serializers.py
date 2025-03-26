e975663cc4c320450d1d73cf81be32e35af0d77b
IsaacSemb <isaacsemb1996@gmail.com>
2025-03-26 21:18:57 +0000

    using model serializers to neat pick out hte fields we want
    you can use the __all__ string but its a bad practice

--- djg_shop/shop/serializers.py
+++ djg_shop/shop/serializers.py
@@ -21,28 +21,55 @@ class CategorySerializer(serializers.Serializer):
     id = serializers.IntegerField()
     category_name = serializers.CharField(max_length=255)
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    product_name = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
+class ProductSerializer(serializers.ModelSerializer):
     
-    # you can calculate new fields
+    
+    # category = serializers.HyperlinkedRelatedField(
+    #     queryset = Category.objects.all(),
+    #     view_name = 'category-detail'        
+    # )
+    
+    # price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
     price_with_tax = serializers.SerializerMethodField('calculate_tax')
     
-    # this returns id
-    category = serializers.PrimaryKeyRelatedField( queryset = Category.objects.all())
     
-    # this returns the real name of category -- this has to be same name as field -- unless you do source source='category' 
-    category = serializers.StringRelatedField() 
+    # py default model serializers use pk field for relations
     
-    # you can even return a whole object by another serializer --- nesting
-    category = CategorySerializer()
+    # you can always retain modifications
     
-    # setting it as a link back home to categories
-    category = serializers.HyperlinkedRelatedField(
-        queryset = Category.objects.all(),
-        view_name = 'category-detail'        
-    )
+    class Meta:
+        model = Product
+        fields = [ 'id', 'product_name', 'unit_price', 'category', 'price_with_tax' ]
+        
+        # this is a bad practice so i hear
+        # fields = '__all__' 
+
+
+
+    
+    
+    
+    # id = serializers.IntegerField()
+    # product_name = serializers.CharField(max_length=255)
+    # price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
+    
+    # # you can calculate new fields
+    # price_with_tax = serializers.SerializerMethodField('calculate_tax')
+    
+    # # this returns id
+    # category = serializers.PrimaryKeyRelatedField( queryset = Category.objects.all())
+    
+    # # this returns the real name of category -- this has to be same name as field -- unless you do source source='category' 
+    # category = serializers.StringRelatedField() 
+    
+    # # you can even return a whole object by another serializer --- nesting
+    # category = CategorySerializer()
+    
+    # # setting it as a link back home to categories
+    # category = serializers.HyperlinkedRelatedField(
+    #     queryset = Category.objects.all(),
+    #     view_name = 'category-detail'        
+    # )
     
     
     def calculate_tax(self, product:Product):
