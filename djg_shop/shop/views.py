f27d1077cddef0d6d2f182be3828fa13144ce0e5
IsaacSemb <isaacsemb1996@gmail.com>
2025-04-01 23:57:21 +0100

    implement put method with serilaizers to edit data and save it to database

--- djg_shop/shop/views.py
+++ djg_shop/shop/views.py
@@ -34,19 +34,23 @@ retrieve the object and pass it to the serializer
 
 """
 
-@api_view()
+@api_view(['GET', 'PUT'])
 def product_detail(request,id):
-    
-    # get the product you want to get 
+    # get the product you want to work with 
     product = get_object_or_404(Product, pk=id)
-    
-    # srialisze it using the responsible serializer
-    serializer = ProductSerializer(product)
-
-    # get the data from it and pass it and pass it as a response
-    data = serializer.data
-    
-    return Response(data)
+        
+    if request.method == 'GET':
+        # serialisze it using the responsible serializer
+        serializer = ProductSerializer(product)
+        # get the data from it and pass it and pass it as a response
+        data = serializer.data
+        return Response(data, status=status.HTTP_201_CREATED)
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data = request.data, )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+        
 
 @api_view()
 def category_detail(request, pk): 
