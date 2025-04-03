626c6b0ecdd371d47865fee3dd081830cddb215c
IsaacSemb <isaacsemb1996@gmail.com>
2025-04-03 05:34:30 +0100

    setting reviews to show only the product in the url or requested product

--- djg_shop/shop/views.py
+++ djg_shop/shop/views.py
@@ -22,8 +22,10 @@ from .serializers import ProductSerializer, CategorySerializer, ReviewSerializer
 # example the product and product details
 
 class ReviewViewSet(ModelViewSet):
-    queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+    
+    def get_queryset(self):
+        return Review.objects.filter(product_id = self.kwargs['product_pk'])
     
     def get_serializer_context(self):
         return { 'product_id' : self.kwargs['product_pk'] }
